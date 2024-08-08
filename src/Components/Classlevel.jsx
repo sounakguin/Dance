c851b499f9e5b4153615228286fc5d592ac7db84
Sounak Guin <sounak.guin@gmail.com>
2024-08-08 15:06:46 +0530

    first commit

--- src/Components/Classlevel.jsx
+++ src/Components/Classlevel.jsx
@@ -9,21 +9,21 @@ export default function ClassLevel() {
       <div className="C_box">
         <div className="Allboxs">
           <p>Dance</p>
-          <img src="public/Class_Level_Girl.png" alt="Dance" />
+          <img src="/Class_Level_Girl.png" alt="Dance" />
           <p>$100.00</p>
           <p>Lorem ipsum dolor sit amet</p>
           <button className="join_now_button">Join Now</button>
         </div>
         <div className="Allboxs">
           <p>Basic</p>
-          <img src="public/Class_Level_Girl.png" alt="Basic" />
+          <img src="/Class_Level_Girl.png" alt="Basic" />
           <p>$50.00</p>
           <p>Lorem ipsum dolor sit amet</p>
           <button className="join_now_button">Join Now</button>
         </div>
         <div className="Allboxs">
           <p>Professional</p>
-          <img src="public/Class_Level_Girl.png" alt="Professional" />
+          <img src="/Class_Level_Girl.png" alt="Professional" />
           <p>$500.00</p>
           <p>Lorem ipsum dolor sit amet</p>
           <button className="join_now_button">Join Now</button>
